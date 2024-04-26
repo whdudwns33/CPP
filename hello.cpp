f186c6f6174257b1d0f9234530aa1be07da931e9
jyj051811@gmail.com <jyj051811@gmail.com>
2024-04-26 10:44:49 +0900

    C++ 첫번째 Hello

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  // input/output의 약자
+using namespace std; // std 클래스에 있는 함수들을 사용 하겠다는 의미
+int main(void){
+  string name;
+  cout << "wirte your name : ";
+  cin >> name;
+  cout << name << " Hello." << endl;
+  return 0;
+}
